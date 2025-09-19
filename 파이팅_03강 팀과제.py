bff46cbff727e00ac7e4110732e73a861f434a74
지연 <psdx1234@naver.com>
2025-09-19 11:49:12 +0900

    Add files via upload

---
+++ 파이팅_03강 팀과제.py
@@ -0,0 +1,120 @@
+#2장 팀과제 01번 - 1장 실습문제 1번 자동차 그리기 프로그램 수정하기
+import turtle as t
+t.shape("turtle")
+#사용자에게 자동차크기, 색깔 입력 받기
+size = int(input("자동차 크기는 얼마로 할까요? : "))
+size_c = int(input("바퀴 크기는 얼마로 할까요? : "))
+body_color = input("자동차 몸통 색깔은 어떤 색으로 할까요? : ")
+top_color = input("자동차 상단 색깔은 어떤 색으로 할까요? : ")
+
+# 몸통 그리기
+t.up()
+t.goto(-2 * size, -0.5 * size)
+t.down()
+t.fillcolor(body_color)
+t.begin_fill()
+t.forward(4 * size)
+t.left(90)
+t.forward(2 * size)
+t.left(90)
+t.forward(4 * size)
+t.left(90)
+t.forward(2 * size)
+t.end_fill()
+
+# 상단 그리기
+t.up()
+t.goto(-1 * size, 2.5 * size)
+t.down()
+t.fillcolor(top_color)
+t.begin_fill()
+t.forward(1 * size)
+t.left(90)
+t.forward(2 * size)
+t.left(90)
+t.forward(1 * size)
+t.left(90)
+t.forward(2 * size)
+t.end_fill()
+
+# 바퀴
+t.up()
+t.goto(-1.3 * size, -0.5 * size)
+t.down()
+t.color("black")
+t.begin_fill()
+t.circle(size_c)
+t.end_fill()
+
+t.up()
+t.goto(1.3 * size, -0.5 * size)
+t.down()
+t.color("black")
+t.begin_fill()
+t.circle(size_c)
+t.end_fill()
+
+t.reset()
+
+#2장 팀과제 02번 - 2장 실습문제 2번 움직이는 집 프로그램 수정하기
+
+import turtle as t
+
+t.shape("turtle")
+
+# 사용자 입력
+size = int(input("집의 크기를 입력하세요: ")) 
+body_color = input("집 본체 색깔을 입력하세요: ")
+roof_color = input("지붕 색깔을 입력하세요: ") 
+
+# 본체 그리기 (정사각형)
+t.up()
+t.goto(0, 0)
+t.down()
+
+t.fillcolor(body_color)
+t.begin_fill()
+
+t.forward(size)
+t.left(90)
+t.forward(size)
+t.left(90)
+t.forward(size)
+t.left(90)
+t.forward(size)
+t.left(90)
+
+t.end_fill()
+
+# 지붕 그리기
+t.fillcolor(roof_color)
+t.begin_fill()
+
+t.up()
+t.goto(0, size)
+t.down()
+
+t.goto(size / 2, size * 2)
+t.goto(size, size)
+t.goto(0, size)
+
+t.end_fill()
+
+# 바퀴
+t.fillcolor("brown")
+
+t.up()
+t.goto(size * 0.2, -size * 0.3)
+t.down()
+t.begin_fill()
+t.circle(size * 0.15)
+t.end_fill()
+
+t.up()
+t.goto(size * 0.8, -size * 0.3)
+t.down()
+t.begin_fill()
+t.circle(size * 0.15)
+t.end_fill()
+
+t.done()
