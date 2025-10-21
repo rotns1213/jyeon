e0c8bca3e6a7588ffb976b4071a2ae04dcf35d3d
지연 <psdx1234@naver.com>
2025-10-21 15:58:58 +0900

    Add files via upload

---
+++ FirstWindow.java
@@ -0,0 +1,13 @@
+package sec01;
+
+import javax.swing.*;
+
+public class FirstWindow {
+	public static void main(String[] args) {
+		//윈도우 생성 
+		JFrame f1 = new JFrame();
+		f1.setTitle("첫 윈도우 프로그램");
+		f1.setSize(300,200);
+		f1.setVisible(true);
+	}
+}
