6cb713c5a401ea70a0953f86de62f712f5fc1327
지연 <psdx1234@naver.com>
2025-11-11 09:57:41 +0900

    Add files via upload

---
+++ MouseClass.java
@@ -0,0 +1,30 @@
+package sec02;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MouseClass extends JFrame{
+
+	JLabel lbl;
+	MouseClass(){
+		lbl=new JLabel("cheer up");
+		setLayout(new FlowLayout());
+		lbl.setForeground(Color.red);
+		add(lbl);
+		//2. 리스너 등록
+		addMouseListener(new MyMouse());
+		setSize(500,500);
+		setVisible(true);
+	}
+	//1. 내부 클래스로 이벤트 리스너 작성
+             // 코드 작성
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MouseClass();
+
+	}
+
+}
